de9b8275a7d6e6716b9458d1f2b4847b20de96e8
arianna-cosimini <arianna.cosimini@gmail.com>
2024-01-31 17:21:35 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -7,19 +7,40 @@ for (let i = 1; i <= 100; i++){
     newElement.className ="square";
     newElement.style.backgroundColor="#1389b2";
     newElement.style.border=" 1px solid black";
+    newElement.style.  width= "calc(100% / 10 - 10px)";
+    newElement.style.display="flex";
+    newElement.style.justifyContent="center";
+    newElement.style.alignItems="center";
+    newElement.style.height= "80px";
+    newElement.style.fontSize="20px";
+
     grigliaElement.append(newElement)
     
     if  (i % 5 == 0 && i % 3 == 0){
         newElement.className += " fizzbuzz";
         newElement.innerHTML= "FizzBuzz";
         newElement.style.backgroundColor="#f0466f";
+        newElement.style.fontWeight="bold";
+        newElement.style.color="yellow";
+        newElement.style.fontSize="15px";
+
+0
     } else if (i % 5 == 0){
         newElement.className += " buzz";
         newElement.innerHTML= "Buzz";
         newElement.style.backgroundColor="#f6d667";
+        newElement.style.fontWeight="bold";
+        newElement.style.fontSize="20px"
+        newElement.style.color="red";
+
     } else if (i % 3 == 0){
         newElement.className += " fizz";
         newElement.innerHTML= "Fizz";
         newElement.style.backgroundColor="#0cd6a1";
+        newElement.style.fontWeight="bold";
+        newElement.style.fontSize="25px";
+        newElement.style.color="white";
+
+
     }
-} console.log(grigliaElement)
+}
