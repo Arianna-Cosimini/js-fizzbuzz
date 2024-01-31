67c3608da9a052da41d2d7d875d5918b97b49449
arianna-cosimini <arianna.cosimini@gmail.com>
2024-01-31 16:46:03 +0100

    inserito multipli di 3 e di 5

--- js/script.js
+++ js/script.js
@@ -5,8 +5,20 @@ for (let i = 1; i <= 100; i++){
     const newElement = document.createElement("div");
     newElement.innerHTML= i;
     newElement.className ="square";
+    newElement.style.backgroundColor="#1389b2";
     newElement.style.border=" 1px solid black";
     grigliaElement.append(newElement)
-    // grigliaElement.innerHTML += `<div class="square">${i}</div>`;
-    // grigliaElement.style.border ="1px solid black"
+    
+    if (i % 3 == 0){
+        newElement.className += " fizz";
+        newElement.innerHTML= "Fizz";
+        newElement.style.backgroundColor="#0cd6a1";
+    } else if (i % 5 == 0){
+        newElement.className += " buzz";
+        newElement.innerHTML= "Buzz";
+        newElement.style.backgroundColor="#f6d667";
+    } else if (i % 15 ==0){
+        newElement.className += " fizzbuzz";
+        newElement.innerHTML= "FizzBuzz";
+    }
 } console.log(grigliaElement)
