ba746a7961b88232a074990fb0e466efe0135e36
arianna-cosimini <arianna.cosimini@gmail.com>
2024-01-31 16:57:23 +0100

    inserito multipli sia di 5 che di 3

--- js/script.js
+++ js/script.js
@@ -9,16 +9,17 @@ for (let i = 1; i <= 100; i++){
     newElement.style.border=" 1px solid black";
     grigliaElement.append(newElement)
     
-    if (i % 3 == 0){
-        newElement.className += " fizz";
-        newElement.innerHTML= "Fizz";
-        newElement.style.backgroundColor="#0cd6a1";
+    if  (i % 5 == 0 && i % 3 == 0){
+        newElement.className += " fizzbuzz";
+        newElement.innerHTML= "FizzBuzz";
+        newElement.style.backgroundColor="#f0466f";
     } else if (i % 5 == 0){
         newElement.className += " buzz";
         newElement.innerHTML= "Buzz";
         newElement.style.backgroundColor="#f6d667";
-    } else if (i % 15 ==0){
-        newElement.className += " fizzbuzz";
-        newElement.innerHTML= "FizzBuzz";
+    } else if (i % 3 == 0){
+        newElement.className += " fizz";
+        newElement.innerHTML= "Fizz";
+        newElement.style.backgroundColor="#0cd6a1";
     }
 } console.log(grigliaElement)
